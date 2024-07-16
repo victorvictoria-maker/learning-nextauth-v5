74925b3cbe6a7975ddad64e5adda933735323ea4
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-07-16 10:00:45 +0100

    Understood what callbacks are in authetication and what they are used for, extending sessions and token

--- auth.ts
+++ auth.ts
@@ -1,12 +1,50 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
 
 import { db } from "@/lib/db";
-// const prisma = new PrismaClient();
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  // callbacks are just what happens after user logs in or logs out
+  callbacks: {
+    // a user cant sign if they have not verified their email
+    // async signIn({ user }: any) {
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      console.log({ session });
+      return session;
+    },
+
+    // extend token to session callback by including the id in it to the session
+    // also need to extend the token so we can get the role into the token - create role based access using the middleware and toekn extention
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,11 @@
+import { UserRole } from "@prisma/client";
+import { DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & { role: UserRole };
+
+// so we can type some parts of the session
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
